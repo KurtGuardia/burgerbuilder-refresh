79a115e4825b82809be690a611ca73798ec168ce
Kurt Guardia <kurtguardia@gmail.com>
2021-04-08 21:00:35 +0200

    created folder structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
-import './App.css';
-
+import Layout from './components/Layout/Layout';
 class App extends Component {
   render() {
-    return <div className='App'></div>;
+    return (
+      <div className='App'>
+        <Layout>
+          <p>Test</p>
+        </Layout>
+      </div>
+    );
   }
 }
 
