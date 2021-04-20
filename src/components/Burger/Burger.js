5bb4c1a3b5279df5f47b6761cb71c737d8516297
Kurt Guardia <kurtguardia@gmail.com>
2021-04-20 09:32:29 +0200

    Created the Burger Controls UI

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,11 +3,19 @@ import classes from './Burger.css';
 import BurgerIngredient from './BurgerIndredient/BurgerIngredient';
 
 const Burger = (props) => {
-  let transformedIngredients = Object.keys(props.ingredients).map((igKey) => {
-    return [...Array(props.ingredients[igKey])].map((_, i) => {
-      return <BurgerIngredient key={igKey + i} type={igKey} />;
-    });
-  });
+  let transformedIngredients = Object.keys(props.ingredients)
+    .map((igKey) => {
+      return [...Array(props.ingredients[igKey])].map((_, i) => {
+        return <BurgerIngredient key={igKey + i} type={igKey} />;
+      });
+    })
+    .reduce((arr, el) => {
+      return arr.concat(el);
+    }, []);
+
+  if (transformedIngredients.length === 0) {
+    transformedIngredients = <h3>Please start adding ingredients</h3>;
+  }
 
   return (
     <div className={classes.Burger}>
